0ae0b4c41807244108e75870907d292d2efdee0f
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2017-09-05 21:09:03 +1000

    Added jqi cask

---
+++ Casks/jqi.rb
@@ -0,0 +1,10 @@
+cask 'jqi' do
+  version '1.4.7'
+  sha256 'ac2382f052b264e1084a688d1e72f476f4f462b59ba1d438840853aba211b26a'
+
+  url 'https://www.dropbox.com/s/cjcujamk1nzwmsa/jqi.dmg'
+  name 'jqi'
+  homepage 'https://nire0510.github.io/jqi/'
+
+  app 'jqi.app'
+end
