e871e85acb746fcc29b65c6255cde2aaa5e79510
Concourse CI <concourse@concourse>
2018-07-09 01:10:46 +0000

    Updated cli-bluestrap.rb

--- Formula/cli-bluestrap.rb
+++ Formula/cli-bluestrap.rb
@@ -1,9 +1,9 @@
 class CliBluestrap < Formula
   desc 'cli-bluestrap'
   homepage 'https://bitbucket.org/ffxblue/cli-bluestrap'
-  url 'https://s3-ap-southeast-2.amazonaws.com/cli-bluestrap.ffxblue.com.au/cli-bluestrap-2.3.1.tar.gz'
-  sha256 'c254437abdcf53c2f22a453e2cd087f2589913690bd44900c7ce6e68a0145796'
-  version '2.3.1'
+  url 'https://s3-ap-southeast-2.amazonaws.com/cli-bluestrap.ffxblue.com.au/cli-bluestrap-2.4.0.tar.gz'
+  sha256 '97529ce54af5b8467157377d727e8ff3fd2c9ed10b139260565eeaa28519ce1b'
+  version '2.4.0'
   depends_on 'jq'
   def install
     bin.install "bin/bs"
