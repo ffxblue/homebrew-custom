a38d3687320dcc9a3cbf95d5c9a32a7a591315fd
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2018-07-20 08:46:40 +1000

    Upgrade to saml2aws 2.6.3-beta

--- Formula/saml2aws.rb
+++ Formula/saml2aws.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Saml2aws < Formula
   homepage 'https://github.com/pavelnikolov/saml2aws'
-  url 'https://github.com/pavelnikolov/saml2aws/releases/download/v2.6.2/saml2aws_2.6.2_darwin_amd64.tar.gz'
-  version '2.6.2'
-  sha256 'b4ac4b1bad7b83adc7158626d3564631edfb26023ef72f2a300f7c75ea39684c'
+  url 'https://github.com/pavelnikolov/saml2aws/releases/download/v2.6.3-beta/saml2aws_2.6.2_darwin_amd64.tar.gz'
+  version '2.6.3'
+  sha256 '38ac337780567d42ea145b59bf483769f3b15216e066fa0bb3d8c74a3155dfcb'
 
   depends_on :arch => :x86_64
 
