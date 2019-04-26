b1f7f89b9d17affeff2c56bbd7cf0330e8c7929a
Ben White <hello@bemm.com.au>
2019-04-26 11:31:04 +1000

    Updated cli-bluestrap.rb

--- Formula/cli-bluestrap.rb
+++ Formula/cli-bluestrap.rb
@@ -2,7 +2,7 @@ class CliBluestrap < Formula
   desc 'cli-bluestrap'
   homepage 'https://bitbucket.org/ffxblue/cli-bluestrap'
   url 'https://s3-ap-southeast-2.amazonaws.com/cli-bluestrap.ffxblue.com.au/cli-bluestrap-testing.tar.gz'
-  sha256 'a37338863ebc55d13b3bb04db945a2e4f63ed305d1a66c4822f32bf7c075436a'
+  sha256 '1a482a9b0189a541cf2bb35e0c833bfab7f64752002990058cb9799fafadba76'
   version 'testing'
   depends_on 'jq'
   def install
@@ -12,6 +12,8 @@ class CliBluestrap < Formula
     prefix.install ".bluestrap"
     prefix.install "Brewfile"
     prefix.install "config.json"
+
+    bin.compare_file('Brewfile', 'Brewfile')
   end
   test do
     system 'bs', 'help'
