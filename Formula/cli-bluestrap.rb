60ddb360a625ab4bce2e9c51fba513de8d26919e
Ben White <hello@bemm.com.au>
2019-05-01 19:21:08 +1000

    Updated cli-bluestrap.rb

--- Formula/cli-bluestrap.rb
+++ Formula/cli-bluestrap.rb
@@ -2,26 +2,27 @@ class CliBluestrap < Formula
   desc 'cli-bluestrap'
   homepage 'https://bitbucket.org/ffxblue/cli-bluestrap'
   url 'https://s3-ap-southeast-2.amazonaws.com/cli-bluestrap.ffxblue.com.au/cli-bluestrap-testing.tar.gz'
-  sha256 'b32365b9b866059ca226a63018b479daf0631770e8e805eb9cc21d88a08fb85e'
+  sha256 '6f1a208a402829c51825d25bc4aa185cf1000c479fa2a1dffd6edc3139620aab'
   version 'testing'
   depends_on 'jq'
-  def existing_version
+  def set_existing_version
     if any_version_installed?
-      # Unsure why, but 'installed_version' goes with the version defined above, not the currently installed version
-      # I get the feeling thats because this is a _function_ not a variable definition...
       `#{HOMEBREW_PREFIX}/bin/brew info cli-bluestrap --json | #{HOMEBREW_PREFIX}/bin/jq -r '.[].installed[0].version'`.strip
     else
       version
     end
   end
+  def get_existing_version
+    @existing_version ||= set_existing_version
+  end
   def existing_path
-    "#{HOMEBREW_PREFIX}/Cellar/cli-bluestrap/#{existing_version}"
+    "#{HOMEBREW_PREFIX}/Cellar/cli-bluestrap/#{get_existing_version}"
   end
   def compare_files(file)
     if compare_file("#{prefix}/#{file}", "#{existing_path}/#{file}")
-      @@changed["#{file}"] = false
+      @changed["#{file}"] = false
     else
-      @@changed["#{file}"] = true
+      @changed["#{file}"] = true
     end
   end
   def install
@@ -33,11 +34,11 @@ class CliBluestrap < Formula
     prefix.install "config.json"
   end
   def post_install
-    @@changed = Hash.new
+    @changed = Hash.new
     compare_files(".bluestrap")
     compare_files("Brewfile")
     compare_files("config.json")
-    puts `bs upgrade-check '#{@@changed.to_json}'`
+    puts `bs upgrade-check '#{@changed.to_json}'`
   end
   test do
     system 'bs', 'help'
