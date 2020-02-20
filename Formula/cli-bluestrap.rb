075c19688d3163b31be5e0659ac81d9283ac240a
Ben White <hello@bemm.com.au>
2020-02-20 17:10:53 +1100

    manually update incorrect SHA

--- Formula/cli-bluestrap.rb
+++ Formula/cli-bluestrap.rb
@@ -2,7 +2,7 @@ class CliBluestrap < Formula
   desc 'cli-bluestrap'
   homepage 'https://bitbucket.org/ffxblue/cli-bluestrap'
   url 'https://s3-ap-southeast-2.amazonaws.com/cli-bluestrap.ffxblue.com.au/cli-bluestrap-2.69.0.tar.gz'
-  sha256 '7d35df247aaba87b2397adb873b3a9ab16214ec140dd0a98ad6ccbeb8e240aa9'
+  sha256 '99c05bae985ac48657504527c99cddedeb6e57835db38b1d69b559dad4c8f3e1'
   version '2.69.0'
   depends_on 'jq'
   def install
